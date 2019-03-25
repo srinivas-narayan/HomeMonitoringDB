99f345bcfb840c3b7914efbe259f967c7812c696
xLasercut <tianxun.wang2004@gmail.com>
2019-03-25 12:45:57 +0000

    moved pages to components

--- clinicianapp/src/views/Home.vue
+++ clinicianapp/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <a class="nhsuk-skip-link" href="#maincontent">Skip to main content</a>
+        <nhs-banner></nhs-banner>
+        
+
+   
+
+        <div class="nhsuk-width-container">
+            <main class="nhsuk-main-wrapper" id="maincontent">
+                <main-table></main-table>
+            </main>
+        </div>
+
+        <nhs-footer></nhs-footer>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import NhsBanner from '@/components/NhsBanner.vue'
+    import NhsFooter from '@/components/NhsFooter.vue'
+    import MainTable from '@/components/MainTable.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            NhsBanner,
+            NhsFooter,
+            MainTable
+        }
+    }
 </script>
